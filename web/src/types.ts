d56b0f265aac3050432e294ab9dbd93b5fb9a4a3
xsxqaqq <772694329@qq.com>
2025-06-18 18:58:07 +0800

    new

--- web/src/types.ts
+++ web/src/types.ts
@@ -9,13 +9,27 @@ export interface Message {
 export interface Document {
   id: string;
   filename: string;
+  original_filename: string;
+  custom_filename?: string;
   upload_time: string;
   status: string;
   chunk_count: number;
 }
 
+export interface AdminDocument extends Document {
+  user_id: number;
+  username: string;
+  email: string;
+}
+
 export interface DocumentListResponse {
   documents: Document[];
+}
+
+export interface AdminDocumentListResponse {
+  documents: AdminDocument[];
+  status_code: number;
+  message: string;
 }
 
 export interface DocumentUploadResponse {
